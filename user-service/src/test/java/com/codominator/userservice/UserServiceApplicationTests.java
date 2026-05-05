bf51b1dc55a43f5e24af47564879b6f77a2cf4aa
shahid khan <shahidangus@gmail.com>
2026-05-05 12:42:41 +0530

    create user and device test for auto creating data

--- user-service/src/test/java/com/codominator/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/codominator/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.codominator.userservice;
 
+import com.codominator.userservice.entity.User;
+import com.codominator.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class UserServiceApplicationTests {
+    @Autowired
+    private UserRepository userRepository;
+    public static final int NUMBER_OF_USERS = 200;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @Disabled
+    void createUser() {
+        for (int i = 1; i<= NUMBER_OF_USERS; i++) {
+            User user = User.builder()
+                    .firstname("User" + 1)
+                    .lastname("lastname" + i)
+                    .email("user" + i + "@gmail.com")
+                    .alerting(i % 2 == 0)
+                    .energyAlertThreshold(1000.0 + 1)
+                    .build();
+            userRepository.save(user);
+        }
+        log.info("User Repository created:");
     }
 
 }
