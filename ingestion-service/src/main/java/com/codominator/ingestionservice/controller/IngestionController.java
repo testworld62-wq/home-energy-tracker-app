925e457513186d5b20bd7248db053932e0d3b62b
shahid khan <shahidangus@gmail.com>
2026-05-05 17:25:37 +0530

    added data ingestion method

--- ingestion-service/src/main/java/com/codominator/ingestionservice/controller/IngestionController.java
+++ ingestion-service/src/main/java/com/codominator/ingestionservice/controller/IngestionController.java
@@ -1,9 +1,23 @@
 package com.codominator.ingestionservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codominator.ingestionservice.dto.EnergyUsageDto;
+import com.codominator.ingestionservice.kafka.event.EnergyUsageEvent;
+import com.codominator.ingestionservice.service.IngestionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/ingestion")
 public class IngestionController {
+    private final IngestionService ingestionService;
+
+    public IngestionController(IngestionService ingestionService) {
+        this.ingestionService = ingestionService;
+    }
+    @PostMapping
+    public ResponseEntity<?> createEvent(@RequestBody EnergyUsageDto energyUsageDto) {
+        ingestionService.ingestEnergyUsage(energyUsageDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
